1724667532c22d369392211f19c24874896001cd
maxa1m <max.mironenko3@gmail.com>
2021-02-14 00:38:59 +0200

    First lesson

---
+++ MiddleLevelDevCourse/FirstLesson.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MiddleLevelDevCourse
+{
+    public static class FirstLesson
+    {
+        public static void Run()
+        {
+            Console.WriteLine($"Amount of available memory to allocate: {GetAmountOfAvailableMemory():n0} bytes");
+
+            Console.WriteLine($"Max size of single block available in heap to allocate: {GetMaxSizeOfSingleBlockInHeap():n0} bytes");
+
+            Console.WriteLine($"Amount of available memory to allocate after allocating the block: {GetAmountOfAvailableMemory():n0} bytes");
+
+            Console.WriteLine($"Compare with physically available memory: ~{GetAmountOfRAM():n0} bytes");
+        }
+
+        private static long GetAmountOfAvailableMemory()
+        {
+            // false means don't wait for garbage collection.
+            return GC.GetTotalMemory(false);
+        }
+
+        private static long GetMaxSizeOfSingleBlockInHeap()
+        {
+            var maxBlockLength = int.MaxValue;
+            var wasMaxBlockFound = false;
+            while (!wasMaxBlockFound)
+            {
+                try
+                {
+                    GC.AllocateArray<byte>(maxBlockLength);
+                    wasMaxBlockFound = true;
+                }
+                catch
+                {
+                    maxBlockLength--;
+                }
+            }
+
+            return maxBlockLength;
+        }
+
+        private static long GetAmountOfRAM() => 8589934592;
+    }
+}
---
+++ MiddleLevelDevCourse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiddleLevelDevCourse
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                FirstLesson.Run();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
